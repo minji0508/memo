20c650d5eccdb3b4a9630c69de8201df4e0ec0f7
minji0508 <seominji5827@gmail.com>
2026-08-12 15:20:57 +0900

    refactor : 메모생성(Create)기능 데이터 베이스 연동

--- src/main/java/com/meta/memo/controller/MemoController.java
+++ src/main/java/com/meta/memo/controller/MemoController.java
@@ -3,27 +3,46 @@ package com.meta.memo.controller;
 import com.meta.memo.domain.Memo;
 import com.meta.memo.dto.MemoRequestDto;
 import com.meta.memo.dto.MemoResponseDto;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.support.GeneratedKeyHolder;
+import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.web.bind.annotation.*;
 
+import java.sql.JDBCType;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.*;
 
 @RestController
 @RequestMapping("api/memos")
 public class MemoController {
-    //임시 데이터베이스(내장 메모리인 해시맵 활용)
-    private final Map<Long, Memo> memoList = new HashMap<>();
+    private JdbcTemplate jdbcTemplate;
+
+    public  MemoController(JdbcTemplate jdbcTemplate){
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
 
     @PostMapping
     public MemoResponseDto createMemo(@RequestBody MemoRequestDto memoRequestDto){
         //RequestDto -> Entity 변환
         Memo memo = new Memo(memoRequestDto);
 
-        //(임시) 현재 저장된 Memo들의 최대 id 체크
-        Long maxId = memoList.size() >0 ? Collections.max(memoList.keySet()) + 1 : 1;
-        memo.setId(maxId);
 
         // DB 저장
-        memoList.put(memo.getId(), memo);
+        KeyHolder keyHolder = new GeneratedKeyHolder(); // 기본키(id)를 반환 받기 위한 객체
+
+        String sql = "INSERT INTO memo (username, contents) VALUES (?, ?)";
+        jdbcTemplate.update(con-> {
+            PreparedStatement preparedStatement = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
+            preparedStatement.setString(1, memo.getUsername());
+            preparedStatement.setString(2, memo.getContents());
+            return preparedStatement;
+        }, keyHolder);
+
+        // [임시] DB INSERT 후 받아온 기본 키 확인
+        Long id = keyHolder.getKey().longValue();
+        memo.setId(id);
 
         //Entity -> ResponseDto 변환
         MemoResponseDto memoResponseDto = new MemoResponseDto(memo);
